1dc9f9bbe88657604427eb15161a9d4f0f6c6a36
Monika Beiger <mbeiger@sigma.ug.edu.pl>
2015-11-08 14:43:29 +0100

    2. Dodalam klasy

--- main/domain/Producent.java
+++ main/domain/Producent.java
@@ -1,5 +1,60 @@
 package main.domain;
 
 public class Producent {
+	private int idProducent;
+	private String przedsiebiorstwo;
+	private int znizka;
+	private float kwotazamowienia;
+	
+	public Producent() {
+		super();
+	}
 
+	
+	public Producent(String przedsiebiorstwo, int znizka, float kwotazamowienia) {
+		super();
+		this.przedsiebiorstwo = przedsiebiorstwo;
+		this.znizka = znizka;
+		this.kwotazamowienia = kwotazamowienia;
+	}
+	
+	public Producent(int idProducent, String przedsiebiorstwo, int znizka, float kwotazamowienia) {
+		super();
+		this.idProducent = idProducent;
+		this.przedsiebiorstwo = przedsiebiorstwo;
+		this.znizka = znizka;
+		this.kwotazamowienia = kwotazamowienia;
+	}
+
+	public int getIdProducent() {
+		return idProducent;
+	}
+
+	public void setIdProducent(int idProducent) {
+		this.idProducent = idProducent;
+	}
+
+	public String getPrzedsiebiorstwo() {
+		return przedsiebiorstwo;
+	}
+
+	public void setPrzedsiebiorstwo(String przedsiebiorstwo) {
+		this.przedsiebiorstwo = przedsiebiorstwo;
+	}
+
+	public int getZnizka() {
+		return znizka;
+	}
+
+	public void setZnizka(int znizka) {
+		this.znizka = znizka;
+	}
+
+	public float getKwotaZamowienia() {
+		return kwotazamowienia;
+	}
+
+	public void setKwotaZamowienia(float kwotazamowienia) {
+		this.kwotazamowienia = kwotazamowienia;
+	}
 }
--- main/domain/Towar.java
+++ main/domain/Towar.java
@@ -1,5 +1,71 @@
 package main.domain;
 
 public class Towar {
+	private int idTowar;
+	private String model;
+	private int liczba;
+	private float cena;
+	private int idProducent;
+	private Producent producent;
+	
+	public Towar(String model, int liczba, float cena, Producent producent) {
+		super();
+		this.model = model;
+		this.liczba = liczba;
+		this.cena = cena;
+		this.producent = producent;
+		this.idProducent = producent.getIdProducent();
+	}
+
+	public Producent getProducent() {
+		return producent;
+	}
+
+	public void setProducent(Producent producent) {
+		this.producent = producent;
+	}
+
+	public Towar(){
+		super();
+	}
+	
+	public Towar(String model, int liczba, float cena, int idProducent) {
+		super();
+		this.model = model;
+		this.liczba = liczba;
+		this.cena = cena;
+		this.idProducent = idProducent;
+	}
+	
+	public int getIdTowar() {
+		return idTowar;
+	}
+	public void setIdTowar(int idTowar) {
+		this.idTowar = idTowar;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public int getLiczba() {
+		return liczba;
+	}
+	public void setLiczba(int liczba) {
+		this.liczba = liczba;
+	}
+	public float getCena() {
+		return cena;
+	}
+	public void setCena(float cena) {
+		this.cena = cena;
+	}
+	public int getIdProducent() {
+		return idProducent;
+	}
+	public void setIdProducent(int idProducent) {
+		this.idProducent = idProducent;
+	}
 
 }
